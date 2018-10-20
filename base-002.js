c5b113195a8a65a68aef7b18b8f49c36f8ce1766
MaxKnyazev <max.knyazev77@gmail.com>
2018-10-20 12:02:29 +0300

    task 002

---
+++ base-002.js
@@ -0,0 +1,24 @@
+﻿/*
+Base-002.
+
+*/
+
+
+/* ===================   the testing function   ======================= */
+
+function test (testedFunc, result, ...data) {
+    if ( testedFunc(...data) === result ) {
+        console.log('+ + + + + + +');
+        console.log(data);
+        console.log(result);
+    }else{
+        console.log('- - - - - - -');
+        console.log(data);
+        console.log(result);
+    }
+}
+
+
+/* ===================         tests          ======================= */
+
+//test(func, undefined, undefined);

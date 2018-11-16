0651eca048028ea4c2f91faad2b803f32a44e363
MaxKnyazev <max.knyazev77@gmail.com>
2018-11-16 18:41:24 +0300

    base-test-001

---
+++ base-test-001.js
@@ -0,0 +1,57 @@
+// let cat = {
+//     color: 'white',
+//     age: 5,
+//     name: 'Vasil',
+//     height: 30,
+//     impudence: 97,
+//
+//     sayMay: function(){ console.log('May') },
+//     jumpUp: function(n){ console.log(`My height ${n}`) },
+//     setSleep: function(){ console.log('Zzz...')}
+// };
+//
+// cat.sayMay();
+// cat.jumpUp(30);
+// cat.setSleep();
+//
+// let cat2 = {};
+//
+// for (let elem in cat){
+//     cat2[elem] = cat[elem];
+// }
+//
+// cat2.name = 'Mur';
+// cat2.countImpudence = function(){ ++cat2.impudence }
+// cat2.countImpudence();
+//
+// console.log(cat2);
+
+//==========================================================================================================
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let arr2 = arr.map(function(elem, i, array) {
+//     return elem*2;
+// });
+
+// arr.forEach(function(elem, i) {
+//     return arr[i] = elem*2;
+// })
+
+// let str = ['мама', 'мыло', 'раму', 'полдень', 'разлив'];
+//
+// str.sort(function(a, b) {
+//     return a[1] < b[1] ? -1 : 1;
+// })
+// console.log(str);
+
+// let arrSum = arr.reduce(function(sum, elem){
+//     return sum + elem;
+// });
+//
+// let arrSum2 = str.reduce(function(sum, elem){
+//     return sum + elem;
+// });
+//
+// console.log(arrSum2);
+
+//==========================================================================================================
